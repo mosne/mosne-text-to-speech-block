84980788494a61af730af38723111004355dc1fb
Paolo Tesei <ptesei@beapi.fr>
2025-02-23 14:08:51 +0100

    version 0.2.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '874cf1e0d8ba08bffc13');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f1961b551e8094027ec2');
