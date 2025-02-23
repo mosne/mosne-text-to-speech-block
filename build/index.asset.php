b662f1fe58e1fb6cac79d7c0461ff76c8a8b0b00
Paolo Tesei <ptesei@beapi.fr>
2025-02-23 12:41:26 +0100

    refactor button color palette as component

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6161e7be46429508bfe4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6fcbb3d1c5d944b85028');
--- src/components/ButtonColorPopover.js
+++ src/components/ButtonColorPopover.js
@@ -1,51 +1,56 @@
-/**
- * WordPress dependencies
- */
 import {
 	Button,
 	ColorIndicator,
-	ColorPalette,
 	Popover,
+	ColorPalette,
 } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 
-const ButtonColorPopover = ( {
-	currentColor,
-	colors,
-	handeSetAttribute,
-} ) => {
+const ButtonColorPopover = ( { currentColor, colors, onChange, label } ) => {
 	const [ isVisible, setIsVisible ] = useState( false );
 	const toggleVisible = () => {
 		setIsVisible( ( state ) => ! state );
 	};
 
 	return (
-		<>
-			<Button onClick={ toggleVisible } style={ { padding: 0 } }>
-				<ColorIndicator colorValue={ currentColor } />
-			</Button>
-			{ isVisible && (
-				<Popover
-					position="middle left"
-					onFocusOutside={ () => {
-						if ( isVisible ) {
-							setIsVisible( false );
-						}
-					} }
-				>
-					<div style={ { padding: 8 } }>
-						<ColorPalette
-							colors={ colors }
-							clearable={ true }
-							value={ currentColor }
-							onChange={ ( value ) => {
-								handeSetAttribute( value );
-							} }
-						/>
-					</div>
-				</Popover>
-			) }
-		</>
+		<div className="components-base-control">
+			<div className="components-base-control__field">
+				<label className="components-base-control__label">
+					<h3 class="components-truncate components-text components-heading">
+						{ label }
+					</h3>
+					<Button
+						onClick={ toggleVisible }
+						style={ { padding: 0, marginLeft: '8px' } }
+					>
+						<ColorIndicator colorValue={ currentColor } />
+					</Button>
+				</label>
+				{ isVisible && (
+					<Popover
+						position="bottom left"
+						onFocusOutside={ () => {
+							if ( isVisible ) {
+								setIsVisible( false );
+							}
+						} }
+					>
+						<div style={ { padding: 8 } }>
+							<ColorPalette
+								colors={ colors }
+								value={ currentColor }
+								onChange={ ( value ) => {
+									onChange( value );
+									setIsVisible( false );
+								} }
+								clearable={ true }
+							/>
+						</div>
+					</Popover>
+				) }
+			</div>
+		</div>
 	);
 };
-export { ButtonColorPopover };
+
+export default ButtonColorPopover;
