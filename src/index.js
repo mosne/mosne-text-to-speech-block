6f5c14bf641e8e15cd96f79ba6eaaa5f55c7cf17
Paolo Tesei <ptesei@beapi.fr>
2025-03-27 14:23:41 +0100

    feat new icon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
  */
 import { registerBlockType } from '@wordpress/blocks';
-import { SVG, Path } from '@wordpress/components';
+import { SVG, Path, Circle } from '@wordpress/components';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -35,12 +35,12 @@ registerBlockType( metadata.name, {
 				viewBox="0 0 24 24"
 				fill="none"
 			>
+				<Circle cx="12" cy="12" r="12" fill="#FF7A00" />
 				<Path
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					strokeWidth="1.5"
-					stroke="#000000"
-					d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+					fill="#181818"
+					fillRule="evenodd"
+					d="m11.388 7.908-2.72 2.72a.578.578 0 0 1-.408.17h-.917v2.31h.917c.153 0 .3.062.408.17l2.72 2.72v-8.09Zm-.817-.817c.728-.728 1.973-.213 1.973.817v8.09c0 1.03-1.245 1.545-1.973.818l-2.55-2.551h-.678a1.156 1.156 0 0 1-1.155-1.156v-2.312c0-.638.517-1.155 1.155-1.155h.677l2.551-2.551Zm4.664.776a.578.578 0 0 1 .817 0 5.779 5.779 0 0 1 0 8.172.578.578 0 1 1-.817-.817 4.623 4.623 0 0 0 0-6.538.578.578 0 0 1 0-.817ZM13.601 9.5a.578.578 0 0 1 .817 0 3.467 3.467 0 0 1 0 4.904.578.578 0 0 1-.817-.817 2.311 2.311 0 0 0 0-3.27.578.578 0 0 1 0-.817Z"
+					clipRule="evenodd"
 				/>
 			</SVG>
 		),
